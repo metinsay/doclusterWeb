5e2ebfe8e3f01fc0a3238d7ec3976a3982d485b8
metinsay <metinnsay@gmail.com>
2017-08-20 17:46:26 -0400

    Header components

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+@Component({
+  selector: 'app-header',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
